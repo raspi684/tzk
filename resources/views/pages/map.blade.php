774910b2b910c301d2d39639f0b58b576d88fe94
Raspi684 <kontakt@dominikwawrzynczak.pl>
2019-12-08 13:23:05 +0100

    Cookiebot google maps fix

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -11,7 +11,7 @@
 
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.864260716919!2d19.1055649249778!3d50.81461537360082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4710b5d7b7f840c7%3A0xcc31600bad675a19!2sTechniczne%20Zak%C5%82ady%20Naukowe%20im.%20gen.%20W%C5%82adys%C5%82awa%20Sikorskiego!5e0!3m2!1spl!2spl!4v1575212270525!5m2!1spl!2spl"
-                frameborder="0" allowfullscreen="true"></iframe>
+                frameborder="0" allowfullscreen="true" data-cookieconsent="necessary"></iframe>
 
         </div>
     </div>
